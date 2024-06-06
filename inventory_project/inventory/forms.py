f2bf34af478dc71abebedf419d34ab4555b2f5f3
Martobgz <118079877+Martobgz@users.noreply.github.com>
2024-06-06 06:29:58 +0300

    fixes and add-ons
    fixes and add-ons

--- inventory_project/inventory/forms.py
+++ inventory_project/inventory/forms.py
@@ -1,7 +1,17 @@
 from django import forms
-from .models import InventoryItem
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Category, InventoryItem
+
+class UserRegisterForm(UserCreationForm):
+	email = forms.EmailField()
+
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'password1', 'password2']
 
 class InventoryItemForm(forms.ModelForm):
-    class Meta:
-        model = InventoryItem
-        fields = ['name', 'serial_number', 'user']
+	category = forms.ModelChoiceField(queryset=Category.objects.all(), initial=0)
+	class Meta:
+		model = InventoryItem
+		fields = ['name', 'quantity', 'category']
--- inventory_project/inventory/views.py
+++ inventory_project/inventory/views.py
@@ -1,25 +1,81 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import user_passes_test
-from .forms import InventoryItemForm
-from models import InventoryItem
-from django.shortcuts import get_object_or_404
-@user_passes_test(lambda u: u.is_superuser)
-def add_inventory(request):
-    if request.method == 'POST':
-        form = InventoryItemForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('inventory_list')
-    else:
-        form = InventoryItemForm()
-    return render(request, 'inventory/add_inventory.html', {'form': form})
-def inventory_detail(request, pk):
-    item = get_object_or_404(InventoryItem, pk=pk)
-    return render(request, 'inventory/inventory_details.html', {'item': item})
-@user_passes_test(lambda u: u.is_superuser)
-def delete_inventory(request, pk):
-    item = get_object_or_404(InventoryItem, pk=pk)
-    item.delete()
-    return redirect('inventory_list')
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, View, CreateView, UpdateView, DeleteView
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import UserRegisterForm, InventoryItemForm
+from .models import InventoryItem, Category
+from inventory_project.settings import LOW_QUANTITY
+from django.contrib import messages
 
+class Index(TemplateView):
+	template_name = 'inventory/index.html'
 
+class Dashboard(LoginRequiredMixin, View):
+	def get(self, request):
+		items = InventoryItem.objects.filter(user=self.request.user.id).order_by('id')
+
+		low_inventory = InventoryItem.objects.filter(
+			user=self.request.user.id,
+			quantity__lte=LOW_QUANTITY
+		)
+
+		if low_inventory.count() > 0:
+			if low_inventory.count() > 1:
+				messages.error(request, f'{low_inventory.count()} items have low inventory')
+			else:
+				messages.error(request, f'{low_inventory.count()} item has low inventory')
+
+		low_inventory_ids = InventoryItem.objects.filter(
+			user=self.request.user.id,
+			quantity__lte=LOW_QUANTITY
+		).values_list('id', flat=True)
+
+		return render(request, 'inventory/dashboard.html', {'items': items, 'low_inventory_ids': low_inventory_ids})
+
+class SignUpView(View):
+	def get(self, request):
+		form = UserRegisterForm()
+		return render(request, 'inventory/signup.html', {'form': form})
+
+	def post(self, request):
+		form = UserRegisterForm(request.POST)
+
+		if form.is_valid():
+			form.save()
+			user = authenticate(
+				username=form.cleaned_data['username'],
+				password=form.cleaned_data['password1']
+			)
+
+			login(request, user)
+			return redirect('index')
+
+		return render(request, 'inventory/signup.html', {'form': form})
+
+class AddItem(LoginRequiredMixin, CreateView):
+	model = InventoryItem
+	form_class = InventoryItemForm
+	template_name = 'inventory/item_form.html'
+	success_url = reverse_lazy('dashboard')
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['categories'] = Category.objects.all()
+		return context
+
+	def form_valid(self, form):
+		form.instance.user = self.request.user
+		return super().form_valid(form)
+
+class EditItem(LoginRequiredMixin, UpdateView):
+	model = InventoryItem
+	form_class = InventoryItemForm
+	template_name = 'inventory/item_form.html'
+	success_url = reverse_lazy('dashboard')
+
+class DeleteItem(LoginRequiredMixin, DeleteView):
+	model = InventoryItem
+	template_name = 'inventory/delete_item.html'
+	success_url = reverse_lazy('dashboard')
+	context_object_name = 'item'
